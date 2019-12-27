e700f8302735179885a583b4f2b317975ffe5620
OksanaAchkasova <achkasova.oksana@gmail.com>
2019-12-27 20:01:23 +0200

    task1 edited, task2 done

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,26 +1,23 @@
-let minMailLenght = 5,
-    minPassLenght = 6,
-    userLogin = false,
-    currentUserEmail,
-    currentUserPassword;
-    
-let usersAccounts = {
+const MIN_MAIL_LENGHT = 5,
+    MIN_PASS_LENGTH = 6;
+
+let userLogin = false,
+    usersAccounts = {
         'user@gmail.com': 'UserPass',
         'admin@gmail.com': 'AdminPass'
     };
 
-currentUserEmail = prompt('Input your email'); 
-
+let currentUserEmail = prompt('Input your email'); 
 if(currentUserEmail === '' || currentUserEmail === null) {
     alert('Canceled');
-} else if(currentUserEmail.length < minMailLenght) {
+} else if(currentUserEmail.length < MIN_MAIL_LENGHT) {
     alert('I don\'t know any emails having name length less than 5 symbols');
 } else {
     if(currentUserEmail in usersAccounts) {
-        currentUserPassword = prompt('Input your password');
+        let currentUserPassword = prompt('Input your password');
         if(currentUserPassword === '' || currentUserPassword === null) {
             alert('Canceled');
-        } else if(currentUserEmail in usersAccounts && currentUserPassword === usersAccounts[currentUserEmail]) {
+        } else if(currentUserPassword === usersAccounts[currentUserEmail]) {
             userLogin = true;            
         } else {
             alert('Wrong password');
@@ -34,24 +31,27 @@ if(userLogin) {
    if (!confirm('Do you want to change your password?')) {
         alert('You have failed the change.');
     } else {
-        currentUserPassword = prompt('Input your old password');
+        let currentUserPassword = prompt('Input your old password');
         if(currentUserPassword === '' || currentUserPassword === null) {
             alert('Canceled');
-        } else if(currentUserEmail in usersAccounts && currentUserPassword === usersAccounts[currentUserEmail]) {
+        } else if(currentUserPassword === usersAccounts[currentUserEmail]) {
                 let newUserPassword = prompt('Input your new password');
                 if(newUserPassword === '' || newUserPassword === null) {
                     alert('Canceled');
-                } else if(newUserPassword.length < minPassLenght) {
+                } else if(newUserPassword.length < MIN_PASS_LENGTH) {
                     alert('It’s too short password. Sorry.');
                 } else {
                     let newUserPassword2 = prompt('Please input your new password again');
-                    if(newUserPassword2 === newUserPassword) {
-                        usersAccounts[currentUserEmail] = newUserPassword;
-                        alert('You have successfully changed your password');                        
+                    if(newUserPassword2 === '' || newUserPassword2 === null) {
+                        alert('Canceled');
+                    } else if(newUserPassword2 === newUserPassword) {
+                            alert('You have successfully changed your password');                        
                     } else {
-                       alert('You wrote the wrong password'); 
-                    }
+                        alert('You wrote the wrong password'); 
+                    }                   
                 }
+        } else {
+            alert('Wrong password');
         }
     }
 }
--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,49 @@
-// Your code goes here
+const MAX_RANDOM = 8,
+      MAX_RANDOM_2 = 12,
+      MAX_ATTEMPTS = 3;
+    
+let userContinue = true,   
+    totalPrize = 0,
+    attemptPrize = [100, 50, 25],
+    guessed = false;
+    
+if(!confirm('Do you want to play a game?')) {
+    alert('You did not become a billionaire, but can.');
+} else {
+    do {
+        let randomNum = Math.floor(Math.random() * MAX_RANDOM);
+        for(let i = 0, attempts = 3; i < MAX_ATTEMPTS; i++, attempts--) {
+            let userNum = parseInt(prompt(`Choose a roulette pocket number from 0 to 8\nAttempts left: ${attempts}
+Total prize: ${totalPrize}$\nPossible prize on current attempts: ${attemptPrize[i]}$`), 10);
+            if(userNum === randomNum) {
+                totalPrize += attemptPrize[i];
+                alert(`Congratulation, you won! Your prize is: ${totalPrize}$.`);
+                guessed = true;
+                break;
+            } else {
+                alert(`Thank you for your participation. Your prize is: ${totalPrize} $`);            
+            }
+        }
+        userContinue = confirm('Do you want to continue?');
+
+        if(guessed && userContinue) {
+            randomNum = Math.floor(Math.random() * MAX_RANDOM_2);
+            for(let i = 0, attempts = 3; i < MAX_ATTEMPTS; i++, attempts--) {
+                let userNum = parseInt(prompt(`Choose a roulette pocket number from 0 to 12\nAttempts left: ${attempts}
+Total prize: ${totalPrize}$\nPossible prize on current attempts: ${attemptPrize[i]*2}$`), 10);
+                if(userNum === randomNum) {
+                    totalPrize += attemptPrize[i] * 2;
+                    alert(`Congratulation, you won! Your prize is: ${totalPrize}$.`);
+                    break;
+                } else {
+                    alert(`Thank you for your participation. Your prize is: ${totalPrize} $`);            
+                }
+            }
+            userContinue = confirm('Do you want to continue?');
+        }  
+        if(!userContinue) {
+            alert(`Thank you for your participation. Your prize is: ${totalPrize} $`);
+            userContinue = confirm('Do you want to continue?');
+        }
+    } while (userContinue);  
+}    
